fdb9b45a48797a5a429948a8d787e3e20b2b9178
empathicqubit <empathicqubit@entan.gl>
2020-09-11 20:59:48 -0700

    Goattracker

---
+++ goattracker2/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pilotfiber.dl.sourceforge.net/project/goattracker2/GoatTracker%202/2.74/GoatTracker_2.74.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'goattracker2*'
+  checksum      = '91A859A70E0F3524ADEBCE2D28F00289DB1A93344F126B5DA6E22A8BD646F249'
+  checksumType  = 'sha256'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
