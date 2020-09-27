cf13ff41c9ccd80e7e12c4b5e023e4702933e606
empathicqubit <empathicqubit@entan.gl>
2020-09-26 22:37:41 -0400

    Update winvice version. Add csproj for Linuxy builds.

--- winvice-nightly/tools/chocolateyinstall.ps1
+++ winvice-nightly/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://vice.pokefinder.org/dl.php?file=GTK3VICE-3.4-win64-r38557.zip'
+$url        = 'https://vice.pokefinder.org/dl.php?file=GTK3VICE-3.4-win64-r38635.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'winvice*'
-  checksum      = 'B8FD323619B288FECD2D5A4BB3DB49FDE6FA0B6E946961DA98CE29FCE131CA37'
+  checksum      = 'dc2565382d7bb87195d78f2ed774919903f7f7c6c776f9539aab2672c96de858'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
