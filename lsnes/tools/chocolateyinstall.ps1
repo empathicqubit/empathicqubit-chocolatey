d74afcda10c7dd3be1e06a52b0090ca60871a6e3
empathicqubit <empathicqubit@entan.gl>
2021-03-20 00:15:59 -0700

    lsnes

---
+++ lsnes/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://lsnes.tasbot.net/lsnes-rr2-beta24.7z'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'lsnes*'
+  checksum      = '9d3dc711e22bca6a7246db99ceeb6f380ea0b74ea3cbb09d6b4f5a5a2e3a24f2'
+  checksumType  = 'sha256'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
