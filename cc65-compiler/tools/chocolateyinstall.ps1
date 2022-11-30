62a74f1cfe94f4b076edb9561553f5ba3af97141
empathicqubit <empathicqubit@entan.gl>
2022-11-30 04:18:58 +0100

    add tools directory

---
+++ cc65-compiler/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://sourceforge.net/projects/cc65/files/cc65-snapshot-win32.zip/download'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'cc65*'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
