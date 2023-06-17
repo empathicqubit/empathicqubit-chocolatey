8e195cb194e1f542c1a983d790e68a07ce97a6bc
empathicqubit <empathicqubit@entan.gl>
2023-06-17 10:20:57 +0200

    Update goattracker

--- goattracker2/tools/chocolateyinstall.ps1
+++ goattracker2/tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://pilotfiber.dl.sourceforge.net/project/goattracker2/GoatTracker%202/2.74/GoatTracker_2.74.zip'
+$url        = 'https://sourceforge.net/projects/goattracker2/files/GoatTracker%202/2.76/GoatTracker_2.76.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'goattracker2*'
-  checksum      = '91A859A70E0F3524ADEBCE2D28F00289DB1A93344F126B5DA6E22A8BD646F249'
+  checksum      = 'c1b6b159ec0d37ae68599ac83be8934a71cd543e480eb5225f844b62151cea34'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
 }
